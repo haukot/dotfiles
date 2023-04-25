a9c3ae355e90e61bd57c84d67641a3eded3f8ff6
Dmitry Davydov <haudvd@gmail.com>
2023-04-25 23:17:53 +0700

    update untro-tree

--- .emacs.d/configs/evil.el
+++ .emacs.d/configs/evil.el
@@ -3,5 +3,6 @@
 (evil-snipe-mode +1)
 (evil-snipe-override-mode +1)
 
+;; https://github.com/casouri/vundo, undo-fu, undo-fu-session - замены для undo-tree
 (global-undo-tree-mode)
 (evil-set-undo-system 'undo-tree)
